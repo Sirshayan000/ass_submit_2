3d37f067cb25080882e61d054a9aac9277b0d0c3
Sirshayan000 <sirshan000@gmail.com>
2024-10-25 22:57:42 +0500

    assignment_subm_2

--- Components/Footer/Footer.tsx
+++ Components/Footer/Footer.tsx
@@ -1,7 +1,11 @@
-
+import React from "react"
+import styles from "./style3.module.css"
 export default function FooterPage(){
     return(
       <div>
+        <footer className={styles.footer}>
+            <h2 >FOOTER OF THE WEBSITE</h2>
+        </footer>
        
       </div>
     )
--- Components/Hero/Hero.tsx
+++ Components/Hero/Hero.tsx
@@ -1,6 +1,11 @@
+import React from "react"
+import styles from "./style2.module.css"
 export default function HeroPage(){
     return(
       <div>
+        <main className={styles.hero}>
+
+        </main>
         
       </div>
     )
