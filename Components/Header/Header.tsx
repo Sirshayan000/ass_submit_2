aebfc56a568520de324999a63cc931e9fbe70027
Sirshayan000 <sirshan000@gmail.com>
2024-10-25 22:18:47 +0500

    do

--- Components/Header/Header.tsx
+++ Components/Header/Header.tsx
@@ -1,7 +1,20 @@
+import CSS from "../Header/style.module.css"
+
+console.log(CSS)
 export default function HeaderPage(){
     return(
       <div>
-        
+           <header>
+                <div className="navbar">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Contuct US</a></li>
+                        <li><a href="#">DEtails</a></li>
+                        <li><a href="#">More Info</a></li>
+                    </ul>
+
+                </div>
+           </header>
       </div>
     )
   }
